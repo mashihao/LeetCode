eb1a77f6f1cda9adb311a7fb371919d79a7da128
马世豪 <ma_shihao@yeah.net>
2022-10-17 10:21:23 +0800

    第四题 https://leetcode.cn/problems/median-of-two-sorted-arrays/description/

--- src/main/kotlin/findMedianSortedArrays.kt
+++ src/main/kotlin/findMedianSortedArrays.kt
@@ -11,7 +11,7 @@ fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
     val nums = nums1 + nums2
     nums.sort()
     val size = nums.size
-    var result: Double = 0.0
+    var result = 0.0
     if (size % 2 == 0) {
         result = (nums[(size / 2)-1] + nums[size / 2 ]) / 2.0
     } else {
